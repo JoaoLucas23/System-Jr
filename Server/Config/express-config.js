4fd0993a697464de162f9a1e97b74c495ecde1f6
Joao Lucas <jllgoncalves23@gmail.com>
2021-07-12 22:10:03 -0300

    express-config

--- Server/Config/express-config.js
+++ Server/Config/express-config.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
-module.exports = app;
+const cors = require('cors');
+app.use(cors());
 
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+app.use(express.json());
+
+const userRouter = require('../users/controller/user-controller');
+app.use('/users', userRouter);
+
+const carRouter = require('../cars/controller/car-controller');
+app.use('/cars', carRouter);
+
+module.exports = app;
