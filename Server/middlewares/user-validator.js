37e191d455a22cd08e2321cf8b29e7fe54c6da06
Arthur Fernandes <arthursfernandes13@gmail.com>
2021-07-15 10:11:06 -0300

    Revert "Update user-validator.js"
    This reverts commit 94019b0fadd2434bc8236e6a029a9e1584c5efc5.

--- Server/middlewares/user-validator.js
+++ Server/middlewares/user-validator.js
@@ -1,14 +1,1 @@
-const{body} = require('express-validator');
-
-// CreateUser: name, email, user, password, iamge, phone, role
-// Login: Email, password
-//
-
-
-
-
-function getValidations(method) {
-    switch (method) {
-
-    }
-}
+// ola mundo
