e928dbe7d4578881ff423a6494af853344c19cfe
FonsecaGabriela <gabrielaf2308@gmail.com>
2021-07-15 10:06:11 -0300

    validate

--- Server/middlewares/user-validator.js
+++ Server/middlewares/user-validator.js
@@ -1,1 +1,68 @@
-// ola mundo
+const {body, validationResult} = require('express-validator');
+const validate = require('./validate');
+
+function getValidations(method) {
+  switch (method) {
+  case 'login': {
+    return [
+      body('email')
+        .exists()
+        .withMessage('O campo de email deve estar preenchido!')
+        .isEmail()
+        .withMessage('O email inserido não é válido!'),
+      body('password')
+        .exists()
+        .withMessage('Você deve digitar uma senha!')
+        .notEmpty()
+        .withMessage('O campo de senha deve estar preenchido!'),
+    ];
+  };
+  case 'createUser': {
+    return [
+      body('namel')
+        .exists()
+        .withMessage('Você deve enviar um nome!')
+        .isAlpha('pt-BR', {ignore: ' '})
+        .withMessage('Seu nome só pode conter letras!'),
+      body('email')
+        .exists()
+        .withMessage('O campo de email deve estar preenchido!')
+        .isEmail()
+        .withMessage('O email inserido não é válido!'),
+      body('password')
+        .exists()
+        .withMessage('Você deve digitar uma senha!')
+        .notEmpty()
+        .withMessage('O campo de senha deve estar preenchido!'),
+      body('image')
+        .exists()
+        .withMessage('O campo de imagem deve estar preenchido!')
+        .isURL()
+        .withMessage('A imagem deve ser uma URL'),
+    ];
+  };
+  case 'updateUser': {
+    return [
+      body('namel')
+        .optional()
+        .isAlpha('pt-BR', {ignore: ' '})
+        .withMessage('Seu nome só pode conter letras!'),
+      body('email')
+        .optional()
+        .isEmail()
+        .withMessage('O email inserido não é válido!'),
+      body('image')
+        .optional()
+        .isURL()
+        .withMessage('A imagem deve ser uma URL'),
+    ];
+  };
+  }
+}
+
+function userValidate(method) {
+  const validations = getValidations(method);
+  return validate(validations);
+}
+
+module.exports = userValidate;
--- Server/middlewares/validate.js
+++ Server/middlewares/validate.js
@@ -1,1 +1,16 @@
-// ola mundo
+const{validationResult} = require ('express-validator');
+
+function validate(validations){
+    return function(re, res, next){
+        for (const validations of validation){
+            await validation.run(req);
+        }
+        const result = validationResult(req);
+        if(!result.isEmpty(){
+            return res.status(400).json(result.errors);
+        }
+        next();
+    };
+}
+
+module.exports = validate;
