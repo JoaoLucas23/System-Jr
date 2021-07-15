f4db0e0308a6373fa9519d133663ea97dfea8051
Arthur Fernandes <arthursfernandes13@gmail.com>
2021-07-15 10:10:12 -0300

    Update object-filter.js

--- Server/middlewares/object-filter.js
+++ Server/middlewares/object-filter.js
@@ -1,12 +1,12 @@
-function objetcFilter(object, keys){
-    return function(req, res, next){
-      Object.keys(req[object]).forEach((key) => {
-               if(keys.indexOf(key) == -1) {
-                delete key;
-            }
-        });
-        next();
-    };
-  }
+function objetcFilter(object, keys) {
+  return function(req, res, next) {
+    Object.keys(req[object]).forEach((key) => {
+      if (keys.indexOf(key) == -1) {
+        delete key;
+      }
+    });
+    next();
+  };
+}
 
 module.exports = objetcFilter;
