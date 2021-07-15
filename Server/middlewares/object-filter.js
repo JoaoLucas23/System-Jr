d947d7ac7bbd5dfaeec5751af94ca897bb25b8d3
Arthur Fernandes <arthursfernandes13@gmail.com>
2021-07-15 09:50:03 -0300

    Update object-filter.js

--- Server/middlewares/object-filter.js
+++ Server/middlewares/object-filter.js
@@ -1,1 +1,12 @@
-// ola mundo
+function objetcFilter(object, keys){
+    return function(req, res, next){
+      Object.keys(req[object]).forEach((key) => {
+               if(keys.indexOf(key) == -1) {
+                delete key;
+            }
+        });
+        next();
+    };
+  }
+
+module.exports = objetcFilter;
