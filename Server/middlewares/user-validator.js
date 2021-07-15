94019b0fadd2434bc8236e6a029a9e1584c5efc5
Arthur Fernandes <arthursfernandes13@gmail.com>
2021-07-15 09:50:00 -0300

    Update user-validator.js

--- Server/middlewares/user-validator.js
+++ Server/middlewares/user-validator.js
@@ -1,1 +1,14 @@
-// ola mundo
+const{body} = require('express-validator');
+
+// CreateUser: name, email, user, password, iamge, phone, role
+// Login: Email, password
+//
+
+
+
+
+function getValidations(method) {
+    switch (method) {
+
+    }
+}
