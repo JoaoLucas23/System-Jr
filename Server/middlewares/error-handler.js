039746d53938c040632706bc21ad2c66acea60c5
Joao Lucas <jllgoncalves23@gmail.com>
2021-07-15 10:32:28 -0300

    error-handler

--- Server/middlewares/error-handler.js
+++ Server/middlewares/error-handler.js
@@ -1,1 +1,26 @@
-// ola mundo
+const AuthError = require('../errors/AuthError');
+const QueryError = require('../errors/QuerryError');
+const {UniqueConstraintError} = require('sequelize');
+
+function errorHandler(error, req, res, next) {
+  const message = error.message;
+  let status = 500;
+
+  if (error instanceof AuthError) {
+    status = 403;
+  }
+  if (error instanceof QueryError) {
+    status = 406;
+  }
+
+  if (error instanceof UniqueConstraintError) {
+    status = 406;
+    const field = error.errors[0].path.split('.')[1];
+    message = `Ja existe um registro no sistema um registro desse ${field}`;
+  }
+
+  console.log(error);
+  res.status(status).json(message);
+}
+
+module.exports = errorHandler;
