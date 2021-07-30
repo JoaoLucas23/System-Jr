32c136c8b49d8ee1d406f230a74f6a00e73cdf3f
Lorenzo <lorenzobicalho@hotmail.com>
2021-07-30 02:22:38 -0300

    Personalização da página HOME

--- react-app/src/componentes/Home/Home.jsx
+++ react-app/src/componentes/Home/Home.jsx
@@ -1,40 +1,59 @@
-import Logo from '../../assests/logo.jpg';
-import LogoVariant from '../../assests/logoVariant.png';
+import logo from '../../assests/logoreal.png';
 import {Link} from 'react-router-dom';
 import './Home.css'
 
 export default function Home() {
     return (
+        
         <div className="Home">
+
+            <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            </div>            
             <div className="Header">
                 <div className="flex-title">
-                    <img src={Logo}
+                    <img src={logo}
                         alt="logo"
                         width='120px'
-                        height='160px' 
+                        height='120px' 
                         padding='0' 
                     />
                 </div>
+
+                <div className="Titulo">TROPA PISTÃO</div>
+                
                 <div className="flex-button">
                     <Link to="/login">
                         <button className="Login-button">
                             Login
                         </button>
                     </Link>
-                </div>
+                    
+                    <Link to="/login">
+                        <button className="Login-button">
+                            Cadastrar
+                        </button>
+                    </Link>
                 
+                </div>
+
         </div>
         <div className="Conteudo">
             <figure>
                 <img id="LogoV"
-                    src={LogoVariant}
+                    src={logo}
                     alt="variante logo"
                     height='150px'
                     width="300px" 
                 />
-                <figcaption>O maior e mais original site de compras</figcaption>
+                <figcaption>O maior, melhor e mais original site de compras e vendas de carros</figcaption>
             </figure>
         </div>
+
+
         </div>
     )
 }
